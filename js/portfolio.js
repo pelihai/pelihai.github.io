102ddca60636f650fbc8968e51f878b75eab1d02
ProMac <promac@ProMacs-MacBook-Pro.local>
2016-05-29 13:20:04 +0300

    dfdf

--- js/portfolio.js
+++ js/portfolio.js
@@ -42,23 +42,7 @@ $( document ).ready(function() {
 */
 
 
-var fileExt = {};
-    fileExt[0]=".png";
-    fileExt[1]=".jpg";
-    fileExt[2]=".gif";
-$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: '../../Images/Avatar/',
-    success: function (data) {
-       $("#fileNames").html('<ul>');
-       //List all png or jpg or gif file names in the page
-       $(data).find('a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + ")').each(function () {
-           var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-           $("#fileNames").append( '<li>'+filename+<'/li'>);
-       });
-       $("#fileNames").append('</ul>');
-     }     
-  });
+
 
 
 
